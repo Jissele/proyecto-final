73b9515de775e3e1c7690d34a00aa66b8a92ad82
Sebastián Barra <s.andresbarra@gmail.com>
2023-12-16 17:24:01 -0300

    subida Json

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from './Context/Provider';
 import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
